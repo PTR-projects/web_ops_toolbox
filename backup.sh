fa70397bf79547a7f992132d5136212fc86d1e59
mkarawacki <mapkarawacki@gmail.com>
2023-12-09 12:39:30 +0100

    Updated backup.sh for separate files for each db

--- backup.sh
+++ backup.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 #zrzut baz danych
-mysqldump --all-databases | xz > databasebackup_$(date +%F-%H_%M).xz
+for DB in $(mysql -e 'show databases' -s --skip-column-names); do
+    mysqldump $DB | gzip -c > ./bak/$DB-backup_$(date +%F-%H_%M).sql.gz;
+done
+
+mysqldump --databases p579450_forum p579450_wiki p579450_www -u p579450_dbadmin -p 
 ## kopia wordpressa
-#tar -czf ./bak/wordpressbackup_$(date +%F-%H_%M).tar.xz $(echo *.php) $(echo wp-*/)
+tar -czf ./bak/wordpressbackup_$(date +%F-%H_%M).tar.xz $(echo ./public_html/*.php) $(echo ./public_html/wp-*/)
 ## kopia forum
-tar -czf ./bak/forumbackup__$(date +%F-%H_%M).tar.xz forum
+tar -czf ./bak/forumbackup__$(date +%F-%H_%M).tar.xz ./public_html/forum/ -R
 ## kopia wiki
-tar -czf ./bak/wikibackup__$(date +%F-%H_%M).tar.xz wiki
+tar -czf ./bak/wikibackup__$(date +%F-%H_%M).tar.xz ./public_html/wiki/ -R
 ## usuwanie starszych backupów
 #find /bak -mtime +7d -delete #--exec rm {}
