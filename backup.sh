eb27dcdca0e2a1c7a6d07d372a8b5b4c3bce447c
mkarawacki <mapkarawacki@gmail.com>
2023-12-09 13:01:23 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ for DB in $(mysql -e 'show databases' -s --skip-column-names); do
     mysqldump $DB | gzip -c > ./bak/$DB-backup_$(date +%F-%H_%M).sql.gz;
 done
 
-mysqldump --databases p579450_forum p579450_wiki p579450_www -u p579450_dbadmin -p 
+
 ## kopia wordpressa
 tar -czf ./bak/wordpressbackup_$(date +%F-%H_%M).tar.xz $(echo ./public_html/*.php) $(echo ./public_html/wp-*/)
 ## kopia forum
