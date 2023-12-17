5cf30bde6e120ad2228228bd62b8f2805ebe77ca
mkarawacki <mapkarawacki@gmail.com>
2023-12-17 18:29:31 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,9 @@
+<<<<<<< Updated upstream
 #! /bin/bash
 ##usuwanie co tydzień starszych niż tydzień backupów
 find /bak -mtime +7d -delete
+=======
+#! /bin/bash
+##usuwanie co tydzień starszych niż tydzień backupów
+find ./bak -mtime +6 -delete
+>>>>>>> Stashed changes
